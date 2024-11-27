b4d626f9a3e53895cd13434fe72822a6eaf02541
Greninja9257 <threelittlepigs818@gmail.com>
2024-11-27 11:50:04 -0800

    YEAH UPDATE :)

---
+++ main.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // Load the web app from localhost or your build folder
+  mainWindow.loadURL('http://localhost:3000');  // Assuming the backend is running
+  // Alternatively, if you have a build, you can use:
+  // mainWindow.loadFile(path.join(__dirname, 'build', 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
